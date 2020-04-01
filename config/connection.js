f40aaaeea8201cb8f4c9ded276c60690935a225a
Tatum Daily <tgd9019@outlook.com>
2020-04-01 12:57:10 -0500

    Connection Update

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PW,
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PW,
+    database: "burgers_db"
+  });
+}
+
+
 
 
 connection.connect(function(err) {
